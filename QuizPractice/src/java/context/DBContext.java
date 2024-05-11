cc0094836b553afe52ba478f09c0ab72c8673dd3
HE176303 - Nguyen Van Nghi <nghinvhe176303@fpt.edu.vn>
2024-05-11 17:42:49 +0700

    Add project base

---
+++ QuizPractice/src/java/context/DBContext.java
@@ -0,0 +1,28 @@
+package context;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DBContext {
+    protected Connection connection;
+    public DBContext()
+    {
+        try {
+            // Edit URL , username, password to authenticate with your MS SQL Server
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=Y23FA1B1";
+            String username = "sa";
+            String password = "123";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+    
+    public static void main(String[] args) {
+        DBContext t = new DBContext();
+        System.out.println(t);
+    }
+}
