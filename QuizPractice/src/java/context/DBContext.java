fe0681b8e21137e4e6b892eb8e9c25e3cd397696
HE176303 - Nguyen Van Nghi <nghinvhe176303@fpt.edu.vn>
2024-05-11 17:46:11 +0700

    update project base

--- QuizPractice/src/java/context/DBContext.java
+++ QuizPractice/src/java/context/DBContext.java
@@ -11,7 +11,7 @@ public class DBContext {
     {
         try {
             // Edit URL , username, password to authenticate with your MS SQL Server
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=Y23FA1B1";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=QuizPracticeDB";
             String username = "sa";
             String password = "123";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
