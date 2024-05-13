dee4ea85db986e6546baf6e5b6c812e72beb695d
HE176303 - Nguyen Van Nghi <nghinvhe176303@fpt.edu.vn>
2024-05-13 23:17:38 +0700

    Add InvalidPhoneNumberFormatException.java

---
+++ QuizPractice/src/java/exception/InvalidPhoneNumberFormatException.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exception;
+
+/**
+ *
+ * @author nghin
+ */
+public class InvalidPhoneNumberFormatException extends RuntimeException {
+
+    public InvalidPhoneNumberFormatException() {
+        super("Phone number is invalid");
+    }
+
+    public InvalidPhoneNumberFormatException(String message) {
+        super(message);
+    }
+}
