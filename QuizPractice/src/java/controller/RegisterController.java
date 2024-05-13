10da96308b70f7abe2a1f8adaff27d97868bd41b
HE176303 - Nguyen Van Nghi <nghinvhe176303@fpt.edu.vn>
2024-05-13 23:13:29 +0700

    Add RegisterController.java

---
+++ QuizPractice/src/java/controller/RegisterController.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import util.security.Security;
+import util.validation.Validation;
+
+/**
+ *
+ * @author nghin
+ */
+public class RegisterController extends HttpServlet {
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("register.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String firstName = request.getParameter("first-name");
+        String lastName = request.getParameter("last-name");
+        String email = request.getParameter("email");
+        String phone = request.getParameter("phone");
+        String dob = request.getParameter("dob");
+        String gender = request.getParameter("gender");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String cfPassword = request.getParameter("cf-password");
+
+        Validation validation = Validation.getInstance();
+        if (email == null || email.isEmpty() || !validation.CheckFormatEmail(email)) {
+            throw new NullPointerException("Null or empty");
+        }
+        try {
+
+        } catch (Exception e) {
+        }
+
+        request.setAttribute("firstName", firstName);
+        request.setAttribute("lastName", lastName);
+        request.setAttribute("email", email);
+        request.setAttribute("phone", phone);
+        request.setAttribute("dob", dob);
+        request.setAttribute("gender", gender);
+        request.setAttribute("username", username);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
