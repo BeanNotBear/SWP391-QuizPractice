e046ae2fe2d0ae875e5d104b2eb5f583cc0086a1
HE176303 - Nguyen Van Nghi <nghinvhe176303@fpt.edu.vn>
2024-06-13 08:17:08 +0700

    Update lesson controller

--- QuizPractice/src/java/controller/LessonController.java
+++ QuizPractice/src/java/controller/LessonController.java
@@ -9,15 +9,13 @@ import dal.PracticeDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.List;
 
-/**
- *
- * @author Admin
- */
+@WebServlet("/lesson")
 public class LessonController extends HttpServlet {
 
     @Override
