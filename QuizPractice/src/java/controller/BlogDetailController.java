17ade208e810d88e25fab689017be827a66d9d56
HE176303 - Nguyen Van Nghi <nghinvhe176303@fpt.edu.vn>
2024-06-17 11:53:40 +0700

    Modified using json

--- QuizPractice/src/java/controller/BlogDetailController.java
+++ QuizPractice/src/java/controller/BlogDetailController.java
@@ -1,4 +1,3 @@
-
 package controller;
 
 import dal.BlogDAO;
@@ -12,47 +11,57 @@ import java.util.List;
 import model.Blog;
 import model.Category;
 
-
-@WebServlet(name="BlogDetailServlet", urlPatterns={"/blogdetail"})
+@WebServlet(name = "BlogDetailServlet", urlPatterns = {"/blogdetail"})
 public class BlogDetailController extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
 
-    throws ServletException, IOException {
+    private int previous_Id;
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
         response.setContentType("text/html;charset=UTF-8");
-        String id = request.getParameter("pid");
+        String idd = request.getParameter("pid");
+        int id;
+
         BlogDAO dao = BlogDAO.getInstance();
-        Blog blog = dao.getBlogById(id);
+        Blog blog = null;
         Blog blogHot = dao.findNewPost();
         List<Category> listcate = dao.getAllCategories();
-
-        request.setAttribute("detail", blog);
-        request.setAttribute("nee", blogHot);
-        request.setAttribute("listcatego", listcate);
-
-        request.getRequestDispatcher("blogdetail.jsp")
-                .forward(request, response);
+        try {
+            id = Integer.parseInt(idd);
+            previous_Id = id;
+            blog = dao.getBlogById(previous_Id);
+            if (blog != null) {
+                request.setAttribute("detail", blog);
+                request.setAttribute("nee", blogHot);
+                request.setAttribute("listcatego", listcate);
+                request.getRequestDispatcher("/blogdetail.jsp")
+                        .forward(request, response);
+            } else {
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            }
+        } catch (NumberFormatException e) {
+            blog = dao.getBlogById(previous_Id);
+            if (blog != null) {
+                request.setAttribute("detail", blog);
+                request.setAttribute("nee", blogHot);
+                request.setAttribute("listcatego", listcate);
+                request.getRequestDispatcher("/blogdetail.jsp")
+                        .forward(request, response);
+            } else {
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            }
+        }
     }
 
-  
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
     /**
      * Handles the HTTP <code>POST</code> method.
-
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -60,13 +69,10 @@ public class BlogDetailController extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-
-   
     @Override
     public String getServletInfo() {
         return "Short description";
